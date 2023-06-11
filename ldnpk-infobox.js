4c91be5f301c00bbf703252a9ced2db23dad794c
Andy Pearson <andy@londonparkour.com>
2023-06-11 21:50:40 +0100

    Update webcomponent

--- ldnpk-infobox.js
+++ ldnpk-infobox.js
@@ -3,150 +3,109 @@
 // ║                     Add the template                     ║
 // ╚══════════════════════════════════════════════════════════╝
 const template = document.createElement('template');
-template.innerHTML = `
+template.innerHTML =  /* html */` 
+
+    <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet">
     <style>
+
         :host {
+            --backgroundColour: #F3F4F6;
+
             display: flex;
+            flex-direction: column;
             align-items: center;
-            gap: 10px; 
+            gap: 10px;
+            padding: 8px;
+            border-radius: 8px;
+            background: var(--backgroundColour);
+            margin-top:16px;
+            font-size: 0.5rem;
+            padding: 1rem;
+            font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Droid Sans', 'Helvetica Neue', sans-serif;
         }
-        button {
-            --size: 60px;
+
+        #top-row {
+            display: flex;
+            flex-direction: row;
+            width: 100%
+        }
+
+        #bottom-row {
+            --lineColour: black;
+
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            border-top:1px solid var(--lineColour)
+        }
+
+        #category {
+
+        }
+
+        #image {
+            --size: 400px;
             width: var(--size);
             height: var(--size);
-            /* use variables to allow penetration of the shadowDOM */
-            background-color: var(--counterButtonColour, aqua);
-            border-radius: 50%;
-            border: 0;
         }
-        h3 {
-            margin: 0px;
-            font-family: monospace;
+
+        #glyph {
+
+        }
+
+        #title {
+            font-size: 3rem;
+            line-height: 6rem;
+            margin: 0rem;
+        }
+
+        #text-columns {
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            font-size: 1rem;
+            line-height: 2rem;
+            margin: 0rem;
+        }
+        #text-column-one {
+            text: blue;
+            width: 100%
+        }
+        #text-column-two {
+            text: red;
+            width: 100%
         }
     </style>
 
-    <button id="subtract">-</button>
-    <h3></h3>
-    <button id="add">+</button>
+    <div id="top-row">
+        <div id="category">category</div>
+        <img id="image" src="#" />
+        <img id="glyph" src="#" />
+    </div>
+    <div id="bottom-row">
+        <h3 id="title">Title</h3>
+        <div id="text-columns">
+            <div id="text-column-one"><slot name="text-column-one"></slot></div>
+            <div id="text-column-two"><slot name="text-column-two"></slot></div>
+        </div>
+    </div>
 `;
 
 
 
-class Card extends HTMLElement {
-
+class InfoBox extends HTMLElement {
 
     // ╔══════════════════════════════════════════════════════════╗
     // ║                    Constructor Method                    ║
     // ╚══════════════════════════════════════════════════════════╝
     constructor() {
         super();
-
-        // set some consts
-        // const template = document.getElementById('template');
-        
         const clone = template.content.cloneNode(true);
-
-        // bind the 'this' context to your components functions
-        this.add = this.add.bind(this);
-        this.subtract = this.subtract.bind(this);
-
-        // Create a shadowDOM for the component
         this.attachShadow({ mode: "open" });
-
-        // Attach the template clone to the shadowDOM
         this.shadowRoot.appendChild(clone);
 
-        // Set the H3 to the value
-        this.valueHeader = this.shadowRoot.querySelector("h3");
-        this.valueHeader.innerHTML = this.value;
-
-        // Get the buttons from the template
-        this.subtractButton = this.shadowRoot.querySelector("#subtract");
-        this.addButton = this.shadowRoot.querySelector("#add");
     }
 
-
-    // ╔══════════════════════════════════════════════════════════╗
-    // ║             Getters / Setters for attributes             ║
-    // ╚══════════════════════════════════════════════════════════╝
-
-    get value() {
-        return this.getAttribute("value");
-    }
-
-    set value(newValue) {
-        this.setAttribute("value", newValue);
-    }
-
-    get min() {
-        return this.getAttribute("min");
-    }
-
-    set min(newValue) {
-        this.setAttribute("min", newValue);
-    }
-
-    get max() {
-        return this.getAttribute("max");
-    }
-
-    set max(newValue) {
-        this.setAttribute("max", newValue);
-    }
-
-    // ╔══════════════════════════════════════════════════════════╗
-    // ║                     Lifecycle events                     ║
-    // ╚══════════════════════════════════════════════════════════╝
-
-    // Event: What to do when attribute is updated. (change the valueHeader html)
-    attributeChangedCallback(attributeName, oldValue, newValue){ 
-        this.valueHeader.innerHTML = this.value;
-    }
-
-    // Return array of attributes to observe (watch like react)
-    static get observedAttributes(){ 
-        return ["value"]
-    }
-
-    // Event: When element started
-    connectedCallback(){ 
-        this.subtractButton.addEventListener("click", this.subtract);
-        this.addButton.addEventListener("click", this.add);
-    }
-
-    // Event: When element is rmeoved - remove eventlisteners
-    disconnectedCallback(){ 
-        this.subtractButton.removeEventListener("click", this.subtract);
-        this.addButton.removeEventListener("click", this.add);
-    }
-
-
-    // ╔══════════════════════════════════════════════════════════╗
-    // ║                     Custom functions                     ║
-    // ╚══════════════════════════════════════════════════════════╝
-    
-    // custom function -
-    subtract(){
-        // this.value is a string
-        // +this.value converts a string to a number
-        let min = this.getAttribute("min");
-        if (!min){ this.min = 0}
-        if (+this.value > +this.min)
-        {
-            this.value = +this.value - 1
-        }
-    }           
-    
-    // custom function +
-    add(){
-        let max = this.getAttribute("max");
-        if (!max){ this.max = 10}
-
-        if (+this.value < +this.max)
-        {
-            this.value = +this.value + 1
-        }
-    }
 }
 
-customElements.define("ldnpk-counter", Card);
+customElements.define("ldnpk-infobox", InfoBox);
